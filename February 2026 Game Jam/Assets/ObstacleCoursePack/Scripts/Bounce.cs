11fc484b45187e45f050f176225a9ad2830dad3e
Maddox Greenspoon <mgreenspoon19@gmail.com>
2026-02-23 08:37:55 -0500

    2/23/2026

--- February 2026 Game Jam/Assets/ObstacleCoursePack/Scripts/Bounce.cs
+++ February 2026 Game Jam/Assets/ObstacleCoursePack/Scripts/Bounce.cs
@@ -1,33 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-	public float force = 10f; //Force 10000f
-	public float stunTime = 0.5f;
-	private Vector3 hitDir;
+    public float force = 15f;
+    public float upwardBoost = 5f;
+    public float stunTime = 0.5f;
 
-	void OnCollisionEnter(Collision collision)
-	{
-		foreach (ContactPoint contact in collision.contacts)
-		{
-			Debug.DrawRay(contact.point, contact.normal, Color.white);
-			if (collision.gameObject.tag == "Player")
-			{
-				hitDir = contact.normal;
-				collision.gameObject.GetComponent<CharacterControls>().HitPlayer(-hitDir * force, stunTime);
-				return;
-			}
-		}
-		/*if (collision.relativeVelocity.magnitude > 2)
-		{
-			if (collision.gameObject.tag == "Player")
-			{
-				//Debug.Log("Hit");
-				collision.gameObject.GetComponent<CharacterControls>().HitPlayer(-hitDir*force, stunTime);
-			}
-			//audioSource.Play();
-		}*/
-	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with: " + collision.gameObject.name);
+    }
 }
